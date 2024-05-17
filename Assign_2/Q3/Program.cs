db0c86cfaead7df67adab633a0045e497b556430
Aditya5009 <138042821+Aditya5009@users.noreply.github.com>
2024-05-17 23:03:55 +0530

    Assignment 2 Q3

---
+++ Assign_2/Q3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Date d = new Date();
+            Date d1 = new Date();
+            Date d2 = new Date();
+            d.accept();
+            d1.accept();
+            //d.display();
+            // bool b = d.isValid(d);
+            Console.WriteLine("Date is valid: " + d.isValid(d));
+            Console.WriteLine("Date: " + d.ToString());
+
+            int diff = d.yearDifference(d, d1);
+            Console.WriteLine("Difference between years is " + diff);
+
+            d2 = d - d1;
+            Console.WriteLine("Difference is : " + d2);
+        }
+    }
+}
