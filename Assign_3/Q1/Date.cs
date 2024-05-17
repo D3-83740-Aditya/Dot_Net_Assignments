7e1a6454e26c546ae41c5e690957cc397e909962
Aditya5009 <138042821+Aditya5009@users.noreply.github.com>
2024-05-17 23:46:27 +0530

    Assignment 3 Q1

---
+++ Assign_3/Q1/Date.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Date
+    {
+        private int day;
+        private int month;
+        private int year;
+
+        public Date() { }
+
+        public Date(int day, int month, int year)
+        {
+            this.Day = day;
+            this.Month = month;
+            this.Year = year;
+        }
+
+        public int Day { get => day; set => day = value; }
+        public int Month { get => month; set => month = value; }
+        public int Year { get => year; set => year = value; }
+
+        public void accept()
+        {
+            Console.WriteLine("Enter day: ");
+            day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter month: ");
+            month = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void display()
+        {
+            Console.WriteLine();
+            Console.WriteLine(day + "/" + month + "/" + year);
+        }
+
+        public bool isValid(Date d)
+        {
+            try
+            {
+                DateTime date = new DateTime(year, month, day);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return day + "/" + month + "/" + year;
+        }
+
+        /* public int getDay() { return day; }
+         public int getMonth() { return month; }
+         public int getYear() { return year; }*/
+
+        //public int yearDifference(Date d1, Date d2)
+        //{
+        //    return d1.Year - d2.Year;
+        //}
+
+
+        //public static Date operator -(Date d1, Date d2)
+        //{
+        //    Date date = new Date();
+        //    date.year = d1.Year - d2.Year;
+
+        //    return date;
+        //}
+
+    }
+
+}
---
+++ Assign_3/Q1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1;
+            p1 = new Person();
+            p1.accept();
+            p1.display();
+        }
+    }
+}
